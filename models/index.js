b1c376106a944f9def69d0136548507f4f3e67bb
Susan Flanagan <SusanFlanagan7@gmail.com>
2017-01-23 15:51:37 -0500

    up to Finding the Actual Page

--- models/index.js
+++ models/index.js
@@ -42,8 +42,24 @@ const Page = db.define('page', {
   }
 });
 
-function urlMaker (title) {
+
+
   
+
+Page.hook('beforeValidate', function(page, options) {
+  page.urlTitle = generateUrlTitle(page.title);
+})
+
+
+function generateUrlTitle (title) {
+  if (title) {
+    // Removes all non-alphanumeric characters from title
+    // And make whitespace underscore
+    return title.replace(/\s+/g, '_').replace(/\W/g, '');
+  } else {
+    // Generates random 5 letter string
+    return Math.random().toString(36).substring(2, 7);
+  }
 }
 
 
--- routes/wiki.js
+++ routes/wiki.js
@@ -8,20 +8,36 @@ router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
+
 router.get('/add', function(req, res, next){
   res.render('addpage.html');
 });
 
+// router.post('/add', function(req, res, next) {
+//   var page = Page.build({
+//     title: req.body.title,
+//     content: req.body.content
+//   });
+//   page.save();
+//   //res.redirect('/');
+//   res.json(req.body);
+// });
+
 router.post('/', function(req, res, next) {
-  //console.log('body: ', req.body);
   Page.create({
     title: req.body.title,
     content: req.body.content
   })
   .then(function(completed){
-    res.redirect('/wiki/');
+    // res.redirect('/wiki/');
+    res.json(req.body);
   })
   .catch(console.error);
+});
+
+router.get('/:urlTitle', function (req, res, next) {
+  res.send('hit dynamic route at ' + req.params.urlTitle);
+  
 });
 
 module.exports = router;
